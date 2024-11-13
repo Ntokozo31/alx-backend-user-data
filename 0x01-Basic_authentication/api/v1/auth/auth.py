c1a36699d062e4abe87389cfaf5690872663afb3
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-13 19:45:53 +0000

    Authantication class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+This module provides authantication class
+"""
+
+from flask import request
+
+
+class Auth:
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if authantication is required for that path"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Retrive authorization header from request"""
+        if request is None:
+            return None
+        return request.header.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Placeholder for the current user"""
+        return None
