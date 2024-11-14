e36691b743ad97331b9e663db1752312d20616a7
Ntokozo31 <dubentokozo45@yahoo.com>
2024-11-14 15:01:51 +0000

    Add and Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,51 +9,31 @@ from typing import List, TypeVar
 from flask import request
 
 
-user = TypeVar('User')
-
-
 class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Check if authantication is required for that path
-
-        Arguments:
-        path (str): The URL path to check
-        for authentication requirements.
-        excluded_paths (List[str]): A list of paths
-        that are exempt from authentication.
-
-        Returns:
-        bool: Returns False
-        for now (authentication is not required).
+        Check if authantication is required for the given path
         """
-        return False
+        if path is None or excluded_paths is None:
+            return True
+
+        path = path.rstrip('/')
+
+        for excluded_path in excluded_paths:
+            if excluded_path.rstrip('/') == path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
         Retrive authorization header from request
-
-        Arguments:
-        request: The Flask request object (optional).
-        If not provided, defaults to None.
-
-        Returns:
-        str: The 'Authorization' header value,
-        or None if no header is found.
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
         Placeholder for the current user
-
-        Arguments:
-        request: The Flask request object (optional).
-        If not provided, defaults to None.
-
-        Returns:
-        TypeVar('User'): Returns None for now
-        but in the future, this will return the user object.
         """
         return None
